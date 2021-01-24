d4378a98ba87b714b21e7aa082b94b71bda6af79
salima0586 <s.tolonova1@gmail.com>
2021-01-24 10:46:52 +0600

    Home Task 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello world");
+	    String first_value;
+	    final int NUM = 14;
+	    String word = "Word";
+	    first_value = NUM + " " + word;
+
+        System.out.println("Первая переменная: " + first_value
+                + " Вторая переменная: " + NUM
+                + " Третья переменная: " + word);
+
+        if (NUM < 0) {
+            System.out.println("Вы сохранили отрицательное значение");
+        } else if (NUM > 0) {
+            System.out.println("Вы сохранили положительное значение");
+        } else if (NUM == 0) {
+            System.out.println("Вы сохранили ноль");
+        } else {
+            System.out.println("Нет никаких значений");
+        }
     }
 }
